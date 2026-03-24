58d68116b9668f688a7cb5b43103d10c72780bfc
Darlington Chanakira <dmudiwa@live.com>
2026-03-24 16:51:42 +0200

    added dev hosting

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'tony-uncostly-forcefully.ngrok-free.dev'
+    ]
+  },
   plugins: [react()],
-});
+})
